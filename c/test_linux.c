c720b001b38b743e1e7175e4ef04bb7987175e3d
qianle <502427330@qq.com>
2017-10-01 21:57:23 +0800

    the second examble

---
+++ c/test_linux.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This is linux\n");
+	
+	int i = 0;
+	
+	for ( i=0; i<1000; i++ )
+	{
+		printf("i = %d\n", i);
+		
+	}
+	
+	return 0;
+}
---
+++ python/print_test.py
@@ -0,0 +1,3 @@
+i = 0
+for i in range (1000):
+    print(i)

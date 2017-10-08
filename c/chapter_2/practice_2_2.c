a5e49a29e386ec9b43943d6c6022011d973d18ec
qianle <502427330@qq.com>
2017-10-08 23:53:12 +0800

    is a code that needen't to translate

---
+++ c/chapter_2/practice_2_2.c
@@ -0,0 +1,15 @@
+\\what is order?and what is sentence think it carefully\\
+
+#include(stdio.h)
+int main (viod)
+{
+	
+	printf("Packinson's Law:\nWork expands so as to ");
+	printf("fill the time\n");
+	printf("available for its completion.\n");
+	return 0;
+	
+} 
+
+
+\\reply:printf，return is orderpackinson's Law:\nWork expands so as to“is sentence  \

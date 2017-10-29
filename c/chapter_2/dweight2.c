c5a5543122f44d8590f46d3b6e11901d8595a828
qianle <502427330@qq.com>
2017-10-29 22:49:41 +0800

    test

---
+++ c/chapter_2/dweight2.c
@@ -0,0 +1,17 @@
+/* this is a comment          
+ name: pun.c                
+ purpose: printf a bad pun  
+ author: stay white   
+*/ 
+#include<stdio.h>
+
+int main(void)
+{
+	printf("to c,\n");
+	printf("or not to c ,\n");
+	printf("that is a question.\n");
+	
+	return 0;
+	
+}
+

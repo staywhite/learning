1417aef33f8453161591e331ce60c5efb3bb0dca
qianle <502427330@qq.com>
2017-10-29 23:13:00 +0800

    shiyan

---
+++ c/homework/shiyan.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+/*{
+	int x, y, z,number;
+	printf("Enter phone numbeer[(xxx)xxx-xxx]:");
+	fflush(stdout);
+	scanf("%d",&number);
+	
+	x = 404; y = 231; z = 9883;
+    printf("You phone number%d.%d.%d",x,y,z);
+    return 0;	
+}
+{
+	int a, b, c, d, e, f, j, k, l, m, number;
+	printf("Enter the numbers from 1 to 16 in any order:\n");
+	fflush(stdout);			
+	scanf("%d",&number);
+	
+	printf("1   2   3   4\n");
+    printf("5   6   7   8\n");
+    printf("9   10  11 12\n");
+    printf("13  14  15 16\n");
+	a = 10; b = 26; c = 42; d = 58;
+	e = 28; f = 32; j = 36; k = 40;
+	l = 34; m = 34;
+	
+	printf("Row sums: %d %d %d %d\n",a,b,c,d);
+    printf("Column sums:%d %d %d %d\n",e,f,j,k);
+    printf("Diagonal sums:%d %d\n",l,m);
+    return 0;
+
+}*/
+{
+	int a,b,c,d;
+	printf("Enter two fractions separated by a sign:");
+	fflush(stdout);
+	scanf("%d/%d+%d/%d",&a,&b,&c,&d);
+	
+	printf("The sum is 23/20");
+	return 0;
+	
+	
+}
